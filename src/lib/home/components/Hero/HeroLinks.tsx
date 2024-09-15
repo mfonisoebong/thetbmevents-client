f295472be3b96bd884b66e85a30d8f0d8879b96f
Mfoniso <106848541+mfonisoebong@users.noreply.github.com>
2024-09-15 18:02:49 +0100

    fixes: fixed broken link

--- src/lib/home/components/Hero/HeroLinks.tsx
+++ src/lib/home/components/Hero/HeroLinks.tsx
@@ -1,6 +1,6 @@
-import ButtonLink from "@common/components/ButtonLink"
-import { FC } from "react"
-import styles from "./styles.module.css"
+import ButtonLink from "@common/components/ButtonLink";
+import { FC } from "react";
+import styles from "./styles.module.css";
 
 const HeroLinks: FC = () => {
   return (
@@ -8,11 +8,11 @@ const HeroLinks: FC = () => {
       <ButtonLink size="lg" href="/events" variant="secondary">
         Find your next event
       </ButtonLink>
-      <ButtonLink size="lg" href="/organizer/signup" variant="primary">
+      <ButtonLink size="lg" href="/signup/organizer" variant="primary">
         Create an event
       </ButtonLink>
     </div>
-  )
-}
+  );
+};
 
-export default HeroLinks
+export default HeroLinks;
