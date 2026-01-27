b6d0ba1f2656ef4edda82605fa938b91907fe5ed
Ajayi Marvellous <ajayimarvellous777@gmail.com>
2026-01-27 13:20:03 +0100

    feat: confirm auth from server

--- src/app/(dashboard)/layout.js
+++ src/app/(dashboard)/layout.js
@@ -1,11 +1,26 @@
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import Providers from "../../components/layouts/providers";
+import {getEndpoint} from "../../lib/utils";
 
 
   export default async function layout({ children }) {
     const cookieStore = await cookies();
-    if (!cookieStore.get('token')) redirect('/');
+
+    let token;
+
+    if (!(token = cookieStore.get('token'))) redirect('/login');
+
+    const res = await fetch(getEndpoint('/auth/me'), {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${token.value}`,
+      },
+    });
+
+    if (res.status !== 200) {
+      redirect('/login');
+    }
 
   return (
     <Providers>
